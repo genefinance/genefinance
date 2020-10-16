d9941c577023581caaa18b5f9afb85653f838340
ewan-huang <ewan-huang@outlook.com>
2020-10-16 11:55:54 +0800

    use BaseToken contract for deliver/collect tokens

--- migrations/2_deploy_tokens.js
+++ migrations/2_deploy_tokens.js
@@ -1,10 +1,12 @@
 const MvsToken = artifacts.require("MvsToken");
 const DnaToken = artifacts.require("DnaToken");
 const EtpToken = artifacts.require("EtpToken");
+const DlpToken = artifacts.require("DlpToken");
 
 
 //truffle migrate --network mainnet
 //truffle migrate --network rinkeby
+//truffle migrate -f 2 --to 2 --network ropsten
 
 module.exports = async function (deployer, network, accounts) {
 
@@ -12,9 +14,11 @@ module.exports = async function (deployer, network, accounts) {
 
     await deployer.deploy(MvsToken, { from: deployAccount });
 
-    await deployer.deploy(DnaToken, "DNAChain Core Asset", "DNA", { from: deployAccount });
+    await deployer.deploy(DnaToken, "Metaverse DNA Chain Token", "DNA", { from: deployAccount });
 
-    await deployer.deploy(EtpToken, "Metaverse ETP Core Asset", "ETP", { from: deployAccount });
+    await deployer.deploy(EtpToken, "Metaverse ETP Chain Token", "ETP", { from: deployAccount });
+
+    await deployer.deploy(DlpToken, "DLP Token", "DLP", { from: deployAccount });
 
 };
 
